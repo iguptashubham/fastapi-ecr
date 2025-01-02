9c9e4dc12462fccc26d71beeb2934f1d503a5708
iguptashubham <shubhog108@gmail.com>
2025-01-02 21:04:47 +0530

    intial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn 
+class response(BaseModel):
+    question : str
+    answer: str
+    
+
+app = FastAPI()
+
+@app.post('/answer')
+def get_answer(chat:response):
+    question = chat.question
+    answer = chat.answer
+    return {'question':question,
+            'answer':answer}
+    
+if __name__ == "__main__":
+    uvicorn.run(app,port=8001)
